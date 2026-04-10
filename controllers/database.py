1b517a3e592657690ac0d011b0c6287f0a7353cf
miguelossa05-collab <miguelossa05@gmail.com>
2026-04-10 18:01:22 -0500

    Add function to create database and table

--- controllers/database.py
+++ controllers/database.py
@@ -1,1 +1,19 @@
+import sqlite3
 
+def crear_db():
+    conn = sqlite3.connect("inventario.db")
+    cursor = conn.cursor()
+
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS productos (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        nombre TEXT,
+        precio REAL,
+        cantidad INTEGER
+    )
+    """)
+
+    conn.commit()
+    conn.close()
+
+crear_db()
