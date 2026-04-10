7f325b1e2ddd3cece65525d76521bd47690cd69e
miguelossa05-collab <miguelossa05@gmail.com>
2026-04-10 18:00:53 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
 from flask import Flask
+from controllers.producto_controller import producto_bp
+import database
 
 app = Flask(__name__)
 
+app.register_blueprint(producto_bp)
+
 @app.route("/")
 def inicio():
-    return "Inventario funcionando"
+    return "Sistema Inventario Activo"
 
 if __name__ == "__main__":
     app.run(debug=True)
