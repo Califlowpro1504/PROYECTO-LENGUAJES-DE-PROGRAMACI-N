7a24d2d0285d827c7cb5bc9e3353f99db941bdfb
miguelossa05-collab <miguelossa05@gmail.com>
2026-04-10 18:04:08 -0500

    Update producto_controller.py

--- controllers/producto_controller.py
+++ controllers/producto_controller.py
@@ -11,4 +11,31 @@ def productos():
     conn = get_connection()
     productos = conn.execute("SELECT * FROM productos").fetchall()
     conn.close()
-    return str(productos)
+    return render_template("productos.html", productos=productos)
+
+
+@producto_bp.route("/productos/agregar", methods=["POST"])
+def agregar():
+    nombre = request.form["nombre"]
+    precio = request.form["precio"]
+    cantidad = request.form["cantidad"]
+
+    conn = get_connection()
+    conn.execute(
+        "INSERT INTO productos (nombre, precio, cantidad) VALUES (?, ?, ?)",
+        (nombre, precio, cantidad)
+    )
+    conn.commit()
+    conn.close()
+
+    return redirect("/productos")
+
+
+@producto_bp.route("/productos/eliminar/<int:id>")
+def eliminar(id):
+    conn = get_connection()
+    conn.execute("DELETE FROM productos WHERE id = ?", (id,))
+    conn.commit()
+    conn.close()
+
+    return redirect("/productos")
